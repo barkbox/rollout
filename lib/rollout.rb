a561a2d3be1daa469a6a755d0dd329cdd9c5ed32
Keith Corwin <keith@barkbox.com>
2019-10-22 10:23:48 -0400

    oops, fix 'max' arg

--- lib/rollout.rb
+++ lib/rollout.rb
@@ -529,7 +529,7 @@ class Rollout
   end
 
   def get_full_history(feature, max = -1)
-    history = @storage.lrange(history_key(feature), 0, -1) || []
+    history = @storage.lrange(history_key(feature), 0, max) || []
     history.map do |entry|
       parse_history_record(entry)
     end
